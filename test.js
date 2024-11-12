73095f4e04c83aff2771141b14201d4fd2b8466e
DAAtmani <p.anas.atmani@gmail.com>
2024-11-12 11:54:02 +0100

    hgb

---
+++ test.js
@@ -0,0 +1,18 @@
+const { Builder, By, until } = require('selenium-webdriver');
+
+async function runTest() {
+    let driver = await new Builder().forBrowser('chrome').build();
+    try {
+        await driver.get('http://localhost:8080');
+        // Ajoute ici tes assertions et tests Selenium
+    } finally {
+        await driver.quit();
+    }
+}
+
+runTest();
+
+let driver = await new Builder()
+    .usingServer('http://selenium:4444/wd/hub')
+    .forBrowser('chrome')
+    .build();
