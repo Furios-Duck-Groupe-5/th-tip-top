9196e40a59e0f368df0b8daa913da1235868ac15
DAAtmani <p.anas.atmani@gmail.com>
2024-11-12 14:05:15 +0100

    hfb

--- test.js
+++ test.js
@@ -17,3 +17,14 @@ let driver = await new Builder()
     .usingServer('http://selenium:4444/wd/hub')
     .forBrowser('chrome')
     .build();
+
+const { Builder } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+
+let driver = new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options()).build();
+
+driver.get('http://www.google.com').then(() => {
+    console.log('Google opened');
+}).catch(err => {
+    console.error('Error opening browser:', err);
+});    
