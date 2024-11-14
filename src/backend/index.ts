42a54c6b409f7e40a38f43373b4397556ac6c343
MedFcBarca <med_abbad@outlook.fr>
2024-11-14 21:16:11 +0100

    999d

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -79,52 +79,54 @@ app.post('/signup', [
 app.post('/login', async (req: Request, res: Response): Promise<void> => {
     const { email, mot_de_passe } = req.body;
 
+    // Vérifie que l'email et le mot de passe sont fournis
     if (!email || !mot_de_passe) {
         res.status(400).json({ message: 'Email et mot de passe sont requis.' });
-        return;
+        return; // On utilise "return" pour stopper l'exécution
     }
 
     try {
         const result = await pool.query('SELECT * FROM Utilisateur WHERE email = $1', [email]);
 
+        // Si aucun utilisateur n'est trouvé, retourne une erreur
         if (result.rows.length === 0) {
             res.status(401).json({ message: 'Email ou mot de passe incorrect.' });
             return;
         }
 
         const user = result.rows[0];
-        const isPasswordValid = await bcrypt.compare(mot_de_passe, user.mot_de_passe);
 
+        // Vérifie que le mot de passe est correct
+        const isPasswordValid = await bcrypt.compare(mot_de_passe, user.mot_de_passe);
         if (!isPasswordValid) {
             res.status(401).json({ message: 'Email ou mot de passe incorrect.' });
             return;
         }
 
+        // Crée un jeton JWT avec les informations de l'utilisateur
         const payload = {
             userId: user.id_user,
             roleId: user.role_id
         };
-
         const secretKey = process.env.JWT_SECRET_KEY || 'Ki0Ka7E8/LINCNrVraSKs6bRL+U4qfP5U80LryzBEAs=';
-        const accessToken = jwt.sign(payload, secretKey, { expiresIn: '15m' }); // Shorter expiration, e.g., 15 minutes
-        const refreshToken = jwt.sign(payload, secretKey, { expiresIn: '7d' }); // Longer expiration, e.g., 7 days
+        const token = jwt.sign(payload, secretKey, { expiresIn: '9999d' });
 
+        // Exclut le mot de passe des données retournées
         const { mot_de_passe: _, ...userWithoutPassword } = user;
 
+        // Retourne les données de l'utilisateur et le jeton
         res.status(200).json({
             message: 'Connexion réussie!',
             user: userWithoutPassword,
-            accessToken,
-            refreshToken,
+            token,
             roleId: user.role_id
         });
     } catch (error) {
         console.error('Erreur lors de la connexion :', error);
         res.status(500).json({ message: 'Erreur lors de la connexion.' });
-        return;
+        return
     }
 });
-
 
 
 
